83f2068353403fadc73d3046787ef77245153a25
mounika1318 <165458348+mounika1318@users.noreply.github.com>
2024-06-12 12:19:08 +0530

    Update index.js

--- src/components/CartSummary/index.js
+++ src/components/CartSummary/index.js
@@ -1,36 +1,46 @@
 // Write your code here
+
+import Popup from 'reactjs-popup'
+
+import Payment from '../Payment'
+
 import CartContext from '../../context/CartContext'
 
 import './index.css'
 
 const CartSummary = () => (
-    <CartContext.Consumer>
-      {value => {
-        const {cartList} = value
-        let total= 0
-        cartList.forEach(eachCartItem => {
-            total += eachCartItem.price * eachCartItem.quantity
-        })
+  <CartContext.Consumer>
+    {value => {
+      const {cartList} = value
+      const items = cartList.length
+      const totalPrice = cartList.reduce(
+        (acc, item) => acc + item.quantity * item.price,
+        0,
+      )
+      return (
+        <div className="cart-summary-container">
+          <div className="cart-summary-card">
+            <h1 className="cart-items-total-price">
+              Order Total: <span>RS {totalPrice}/-</span>
+            </h1>
+            <p className="cart-items-count">{items} Items in cart</p>
 
-        return (
-            <>
-              <div className="cart-summary-container">
-               <h1 className="order-total-value">
-                 <span className="order-total-label">Order Total:</span> Rs {total} {' '}
-                 /-
-               </h1>
-               <p className="total-items">{cartList.lenght} Items in cart</p>
-               <button type="button" className="checkout-button d-m-none">
-                 CheckOut
-               </button>
-              </div>
-              <button type="button" className="checkout-button d-lg-none">
-                CheckOut
-              </button>
-            </>
-        )
-      }}
-    </CartContext.Consumer>
+            <Popup
+              modal
+              trigger={
+                <button className="checkout-btn" type="button">
+                  Checkout
+                </button>
+              }
+              position="top left"
+            >
+              {close => <Payment close={close} />}
+            </Popup>
+          </div>
+        </div>
+      )
+    }}
+  </CartContext.Consumer>
 )
 
 export default CartSummary
